038b21bc9bc9426850715d9d783ed867822d6cba
Ben Dechrai <ben@dechrai.com>
2024-10-17 13:56:58 +0100

    Another refinement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,138 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
+const checkPermissionPayload = {
+  metadata: {
+    depth: 10,
+  },
+  entity: {
+    type: "bone",
+    id: "Femur",
+  },
+  permission: "eat",
+  subject: {
+    type: "dog",
+    id: "Shadow",
+    relation: "",
+  },
+};
+const makeFriendsPayload = {
+  metadata: {
+    schema_version: "",
+  },
+  tuples: [
+    {
+      entity: {
+        type: "person",
+        id: "The_Farmer",
+      },
+      relation: "friend",
+      subject: {
+        type: "person",
+        id: "Mad_Scientist",
+        relation: "",
+      },
+    },
+  ],
+};
+const breakFriendsPayload = {
+  metadata: {
+    schema_version: "",
+  },
+  tuple_filter: {
+    entity: {
+      type: "person",
+      id: "Mad_Scientist",
+    },
+    relation: "friend",
+    subject: {
+      type: "person",
+      id: "The_Farmer",
+      relation: "",
+    },
+  },
+  attribute_filter: {},
+};
 
 export default function Home() {
+  const [doorOpen, setDoorOpen] = useState(false);
+
+  async function checkPermission() {
+    const response = await fetch(
+      "http://localhost:3476/v1/tenants/dog-and-bone/permissions/check",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(checkPermissionPayload),
+      }
+    );
+    const data = await response.json();
+    setDoorOpen(data.can === "CHECK_RESULT_ALLOWED");
+  }
+
+  async function makeFriends() {
+    const response = await fetch(
+      "http://localhost:3476/v1/tenants/dog-and-bone/data/write",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(makeFriendsPayload),
+      }
+    );
+    await response.json();
+  }
+
+  async function breakFriends() {
+    const response = await fetch(
+      "http://localhost:3476/v1/tenants/dog-and-bone/data/delete",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(breakFriendsPayload),
+      }
+    );
+    await response.json();
+  }
+
+  useEffect(() => {
+    checkPermission();
+    const interval = setInterval(() => {
+      checkPermission();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main>
+      <h1>Dog and Bone</h1>
+      <button onClick={makeFriends}>Make Friends</button>
+      <button onClick={breakFriends}>Break Friends</button>
+      <div className={styles.main}>
+        <div className={styles.grid}>
+          <div className={styles.column}>
+            <Image src="/dog.png" alt="Dog" width={156} height={213} />
+          </div>
+          <div className={`${styles.column} ${styles.middle}`}>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src={doorOpen ? "/door_open.png" : "/door_closed.png"}
+              alt="Door"
+              width={157}
+              height={300}
             />
-          </a>
+          </div>
+          <div className={styles.column}>
+            <Image src="/bone.png" alt="Bone" width={200} height={200} />
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
